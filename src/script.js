ae998f9b821667aae800e52f0578ea3538082263
m-subhan-farooq <m.subhan.ibn.farooq@gmail.com>
2024-12-25 14:00:33 +0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -28,8 +28,7 @@
 //     console.error('Error fetching the weather data:', error);
 //   });
 
-const apiKey = process.env.API_KEY;
-print(apiKey)
+const apiKey = "7e37da0056b64044ac2154013240509";
 
 // Function to get the user's location
 function getLocationAndWeather() {
